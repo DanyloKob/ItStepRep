7d0c951f8c2ccbe8d4be4e82232e1dc5ef6ec94e
taiyoShifrators <taiyo.shifrators@gmail.com>
2025-03-22 09:39:05 +0200

    Ось

--- Urok5.py
+++ Urok5.py
@@ -1,7 +1,29 @@
 class Book:
-    def __init__(self, title = None, author = None):
+    def __init__(self, title=None, author=None):
         self.title = title
         self.author = author
 
-class libroary:
-    def __init__(self, ):
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def addBook(self, b: Book):
+        self.books.append(b)
+
+    def v(self):
+        for i in self.books:
+            print(i.title)
+
+    def removeBook(self, b: Book):
+        self.books.remove(b)
+
+
+obj1 = Book('Book1')
+obj2 = Book('Book2')
+library1 = Library()
+library1.addBook(obj1)
+library1.v()
+library1 = Library()
+library1.addBook(obj2)
+library1.v()
